25b1fa14bd7d67f550533e21a677e204ec4e601f
Tony Yao <yao.tony@outlook.com>
2018-04-07 10:46:32 +1000

    babel loader demo

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import layer from './components/layer/layer.js'
+
+const App =  function(){
+    console.log(layer);
+    alert(111);
+}
+
+new App()
--- webpack.config.js
+++ webpack.config.js
@@ -2,37 +2,31 @@
 var htmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry : {
-        main: './src/script/main.js',
-        a: './src/script/a.js',
-        b: './src/script/b.js',
-        c: './src/script/c.js'
-    },
+    entry : './src/app.js',
     output: {
         path: __dirname + "/dist",
-        filename: 'js/[name]-[hash].js'
+        filename: 'js/[name]-bundle.js'
     },
     plugins:[
         new htmlWebpackPlugin({
             template:'index.html',
-            filename:'a.html',
-            title: 'this is a.html',
-            chunks: ['main','a'],
-            inject: false
-        }),
-        new htmlWebpackPlugin({
-            template:'index.html',
-            filename:'b.html',
-            title: 'this is b.html',
-            chunks: ['b','a'],
-            inject: false 
-        }),
-        new htmlWebpackPlugin({
-            template:'index.html',
-            filename:'c.html',
-            title: 'this is c.html',
-            chunks: ['c','a'],
-            inject: false
+            filename:'index.html',
+            inject: 'body'
         })
-    ]
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude:/node_modules/,
+                use:{
+                    loader: 'babel-loader',
+                    options:{
+                        presets: ['env']
+                    }
+                }
+
+            }
+        ]
+    }
 }
